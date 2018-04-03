3b7922ae7c091446a8299eb2800b5aed9dd6266e
Carles Sanz <csanz.soprasteria@gmail.com>
2018-04-03 14:46:05 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+**SOURCE CODE FOR COURSE:**
+https://laracasts.com/series/learn-vue-2-step-by-step/
---
+++ ten/main.js
@@ -0,0 +1,30 @@
+Vue.component('modal', {
+  template: `
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <slot></slot>
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>`,
+});
+
+new Vue({
+  el: '#root',
+
+  data: {
+    modalIsVisible: false,
+  },
+
+  methods: {
+    showModal() {
+      this.modalIsVisible = true;
+    },
+
+    hideModal() {
+      this.modalIsVisible = false;
+    }
+  }
+});
